92a486a3345f117f7e7929820e30c601ea2ba8fb
Eugene Lee <eugene.lee@live.com>
2020-11-06 22:02:49 -0800

    added game evaluation and reset

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import './App.css';
 
 function TTTButton(props)
 { 
-	console.log(props.id + " owned by " + props.owner)
+	if (props.gameover === true)
+	{
+		if (props.owner === 0)
+		{ return (
+			<button className="GameDisabled">
+				<span className="TextBox"></span>
+			</button>
+		)}
+		else if (props.owner !== props.gamewinner)
+		{ return (
+			<button className="GameLost">
+				<span className="TextBox Gray">{props.owner===1 ? "P1" : "P2"}</span>
+			</button>
+		)}
+		else if (props.hilite === true)
+		{ return (
+			<button className={"Winner" + (props.owner===1 ? "PosOne" : "NegOne")} color="#555522">
+				<span className="TextBox">{props.owner===1 ? "P1" : "P2"}</span>
+			</button>
+		)}
+	}
+	//console.log(props.id + " owned by " + props.owner)
 	if (props.owner !== 0)
 	{
 		return (
-			<button class={"Locked" + (props.owner===1 ? "PosOne" : "NegOne")} color="#555522">
-				<span className="TextBox">{props.owner}</span>
+			<button className={"Locked" + (props.owner===1 ? "PosOne" : "NegOne")} color="#555522">
+				<span className="TextBox">{props.owner===1 ? "P1" : "P2"}</span>
 			</button>
 		)
 	}
-	else
+	else // no owner, free to play
 	{
 		return (
 			<button onClick={()=>props.onClick(props.id)}>
@@ -25,14 +46,30 @@ function TTTButton(props)
 class Form extends Component
 {
 	Grid = []
-	constructor()
+	Hilite = [] // used to select which positions won the game
+	constructor(props) // props may have size?
 	{
-		super()
+		super(props)
+		this.reset = this.reset.bind(this);
 		this.state = {
-			current_player: 1
+			current_player: 1,
+			game_over: false,
+			game_winner: 0,
 		}
+		this.reset()
+	}
+
+	reset()
+	{
+		this.setState({
+			current_player: 1,
+			game_over: false,
+			game_winner: 0,
+		})
 		// initialize grid with raw data
-		console.log("initing")
+		this.Grid = [];
+		this.Hilite = [];
+		//console.log("initing")
 		for (let row = 0; row < 3; row++)
 		{
 			var comp = [] // List of data, accessed via column
@@ -46,29 +83,101 @@ class Form extends Component
 					}
 				)
 			}
-			console.log("comp" + row)
-			console.log(comp)
-			this.Grid.push(comp)
+			//console.log("comp" + row)
+			//console.log(comp)
+			this.Grid.push(comp);
+			this.Hilite.push(new Array(3).fill(false));
 		}
-		console.log("this grid")
-		console.log(this.Grid)
+		//console.log("this grid")
+		//console.log(this.Grid)
 	}
 
 	handleClick(id)
 	{
-		console.log("CLICKED " + id);
+		// check for game condition
+		if (this.state.game_over === true){ return; }
+		//console.log("CLICKED " + id);
 		let column = id % 3;
 		let row = Math.floor(id / 3);
-		console.log(`COORD R${row} C${column}`)
+		//console.log(`COORD R${row} C${column}`)
 		this.Grid[row][column].owner = this.state.current_player
-		console.log("Set " + this.Grid[row][column].id + 
-			" to owned by " + this.state.current_player + " : " +
-			this.Grid[row][column].owner)
+		//console.log("Set " + this.Grid[row][column].id + 
+		//	" to owned by " + this.state.current_player + " : " +
+		//	this.Grid[row][column].owner)
 		this.setState({current_player: this.state.current_player * -1})
+
+		// after a cell is selected, check for win conditions
+		// check all possibilities, since each move can only result in
+		// a single winner and never a tie, but a player may win in more than one way
+		// horizontals, i.e. rows, i.e. first axis
+		let winner = 0; // if zero, no winner.
+		for (let i = 0; i < this.Grid.length; i++)
+		{
+			let row = this.Grid[i];
+			if (this.isAllSame(row))
+			{
+				winner = row[0].owner;
+				this.Hilite[i].fill(true);
+				console.log("ROW WINNER is " + winner + " at row " + i)
+			}
+		}
+		// verticals, i.e. columns, i.e. second axis
+		for (let i = 0; i < this.Grid[0].length; i++)
+		{
+			let column = this.Grid.map((row)=>{return row[i]})
+			if (this.isAllSame(column))
+			{
+				winner = column[0].owner;
+				this.Hilite = this.Hilite.map((row)=>{let r = row; r[i] = true; return r;})
+				console.log("COL WINNER is " + winner + " at col " + i)
+			}
+		}
+		// diagonals
+		let upperLeft = this.Grid.map((elem, index)=>{return elem[index]})
+		if (this.isAllSame(upperLeft))
+		{
+			winner = upperLeft[0].owner;
+			this.Hilite = this.Hilite.map((row, index)=>{let r = row; r[index] = true; return r;})
+			console.log("DIA WINNER is " + winner + " at upperLeft")
+		}
+		let kiara = this.Grid.map((elem, index)=>{return elem[this.Grid.length - 1 - index]})
+		if (this.isAllSame(kiara))
+		{
+			winner = kiara[0].owner;
+			this.Hilite = this.Hilite.map((row, index)=>{let r = row; r[this.Grid.length - 1 -index] = true; return r;})
+			console.log("DIA WINNER is " + winner + " at kiara")
+		}
+		// set game state if we have a winner
+		if (winner !== 0) 
+		{ 
+			this.setState(
+				{
+					game_over: true,
+					game_winner: winner
+				}
+			); 
+			//console.log(this.Grid);
+			//console.log(this.Hilite);
+		}
+	}
+
+/** @param {Array} array 
+ * @description Returns true if all element.owner values are the same in an
+ * array, and also non-zero.
+*/
+	isAllSame(array)
+	{
+		let first = array[0].owner;
+		if (first === 0) { return false; }
+		for (let i = 1; i < array.length; i++)
+		{
+			if (array[i].owner !== first) { return false; }
+		}
+		return true;
 	}
 
 	render() {
-		console.log("rendering...")
+		//console.log("rendering...")
 		return (
 		<div>
 			<table className="App">
@@ -85,6 +194,9 @@ class Form extends Component
 										owner = {column.owner}
 										id = {column.id}
 										onClick = {this.handleClick.bind(this)}
+										gameover = {this.state.game_over}
+										gamewinner = {this.state.game_winner}
+										hilite = {this.Hilite[indr][indc]}
 									>
 									</TTTButton>
 								</td>
@@ -95,6 +207,15 @@ class Form extends Component
 				})
 				}
 			</tbody>
+			</table>
+			<table className="App Short">
+				<tbody>
+					<tr key="0"><td>
+						<button className="Reset" onClick={this.reset.bind(this)}>
+							Reset
+						</button>
+					</td></tr>
+				</tbody>
 			</table>
 		</div>
 		)
